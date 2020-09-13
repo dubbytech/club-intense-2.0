5960a62f25e7de74ef3d19c68bc9cdabf094784b
vastg <vastg@DESKTOP-EIVAT60>
2020-09-12 23:00:05 -0400

    siteTitle

---
+++ src/components/Navbar.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <nav>
+            <v-toolbar>
+                <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+                <v-toolbar-title class="grey--text">
+                    <!--<span class="font-weight-light">Site</span>-->
+                    <span>{{siteTitle}}</span>
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+
+                <!-- dropdown menu -->
+                <v-menu offset-y>
+                    <template v-slot:activator="{ on }">
+                        <!-- <v-btn text slot="activator"> -->
+                        <v-btn text v-on="on">
+                            <v-icon left>expand_more</v-icon>
+                            <span>Menu</span>
+                        </v-btn>
+                    </template>
+                    <v-list>
+                        <!-- v-list-tile is changed to v-list-item -->
+                        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                            <v-list-item-title>{{ link.text }}</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+
+                <v-btn text>
+                    <span>Sign Out</span>
+                    <v-icon right>exit_to_app</v-icon>
+                </v-btn>
+            </v-toolbar>
+
+            <v-navigation-drawer v-model="drawer" app class="primary">
+                <v-list>
+                    <!-- v-list-tile is changed to v-list-item -->
+                    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                        <v-list-item-action>
+                            <v-icon class="white--text">{{ link.icon }}</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
+            </v-navigation-drawer>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            drawer: false,
+            siteTitle: "Idemili United",
+            links: [
+                { icon: 'home', text: 'Home', route: '/' },
+                { icon: 'contacts', text: 'About', route: '/about' },
+            ]
+        }),
+    }
+</script>
+
+<style scoped>
+</style>
