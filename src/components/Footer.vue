38d762c65616389fd0dbee186706c7608a9e06c3
vastg <vastg@DESKTOP-EIVAT60>
2020-09-13 14:35:45 -0400

    modified

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <v-footer color="primary lighten-1" padless>
         <v-row justify="center" no-gutters>
-            <v-btn v-for="link in links" :key="link" router :to="link.route" color="white" text rounded class="my-2"> {{ link.text }}</v-btn>
+            <v-btn v-for="link in footerLinks" :key="link" router :to="link.route" color="white" text rounded class="my-2"> {{ link.text }}</v-btn>
             <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
                 {{ new Date().getFullYear() }} — <strong>{{company}}</strong>
             </v-col>
@@ -12,7 +12,7 @@
 <script>
     export default {
         data: () => ({
-            links: [
+            footerLinks: [
                 { icon: 'home', text: 'Home', route: '/' },
                 { icon: 'contacts', text: 'About', route: '/about' },
                 { icon: 'contacts', text: 'Gallery', route: '/gallery' },
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,8 +12,22 @@
                 <v-btn text class="hidden-sm-and-down" v-for="link in generalLinks" :key="link.text" router :to="link.route">
                     <span>{{ link.text }}</span>
                 </v-btn>
+                <v-dialog v-model="dialog" persistent max-width="290">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn text v-bind="attrs" v-on="on">Login/Register</v-btn>
+                    </template>
+                    <v-card>
+                        <v-card-title class="headline">Use Google's location service?</v-card-title>
+                        <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="green darken-1" text @click="login">Login</v-btn>
+                            <v-btn color="green darken-1" text @click="register">Register</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
             </v-toolbar-title>
-            <v-toolbar-title v-if="authenticated">
+            <v-toolbar-title v-else>
                 <v-btn text class="hidden-sm-and-down" v-for="link in authenticatedlinks" :key="link.text" router :to="link.route">
                     <span>{{ link.text }}</span>
                 </v-btn>
@@ -21,9 +35,44 @@
         </v-toolbar>
 
         <v-navigation-drawer v-model="drawer" absolute temporary app class="primary">
-            <v-list>
+            <v-list v-if="!authenticated">
                 <!-- v-list-tile is changed to v-list-item -->
-                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                <v-list-item v-for="link in generalLinks" :key="link.text" router :to="link.route">
+                    <v-list-item-action>
+                        <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                    <v-list-item-action>
+                        <v-icon class="white--text">contacts</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">
+                            <v-dialog v-model="dialog" persistent max-width="290">
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-btn class="white--text" text v-bind="attrs" v-on="on">Login/Register</v-btn>
+                                </template>
+                                <v-card>
+                                    <v-card-title class="headline">Use Google's location service?</v-card-title>
+                                    <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+                                    <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn color="green darken-1" text @click="login">Login</v-btn>
+                                        <v-btn color="green darken-1" text @click="register">Register</v-btn>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+            <v-list v-else>
+                <!-- v-list-tile is changed to v-list-item -->
+                <v-list-item v-for="link in authenticatedlinks" :key="link.text" router :to="link.route">
                     <v-list-item-action>
                         <v-icon class="white--text">{{ link.icon }}</v-icon>
                     </v-list-item-action>
@@ -33,6 +82,8 @@
                 </v-list-item>
             </v-list>
         </v-navigation-drawer>
+
+
     </div>
 </template>
 
@@ -40,26 +91,35 @@
     export default {
         data: () => ({
             drawer: false,
-            siteTitle: "Idemili United",
+            siteTitle: "Organization Name",
             src: "https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg",
-            authenticated: true,
+            dialog: false,
+            authenticated: false,
             authenticatedlinks: [
                 { icon: 'home', text: 'Home', route: '/' },
                 { icon: 'contacts', text: 'About', route: '/about' },
                 { icon: 'contacts', text: 'Gallery', route: '/gallery' },
                 { icon: 'contacts', text: 'Member Only', route: '/member' },
                 { icon: 'contacts', text: 'Admin', route: '/admin' },
-                { icon: 'contacts', text: 'Contact', route: '/contact' },
-                { icon: 'contacts', text: 'Login/Register', route: '/loginRegister' }
+                { icon: 'contacts', text: 'Contact', route: '/contact' }
             ],
             generalLinks: [
                 { icon: 'home', text: 'Home', route: '/' },
                 { icon: 'contacts', text: 'About', route: '/about' },
                 { icon: 'contacts', text: 'Gallery', route: '/gallery' },
-                { icon: 'contacts', text: 'Contact', route: '/contact' },
-                { icon: 'contacts', text: 'Login/Register', route: '/loginRegister' }
+                { icon: 'contacts', text: 'Contact', route: '/contact' }
             ]
         }),
+        methods: {
+            login: function () {
+                this.authenticated = true;
+                this.dialog = false;
+            },
+            register: function () {
+                this.authenticated = true;
+                this.dialog = false;
+            }
+        }
     }
 </script>
 
