aa31131e9f7aacad4d49f0059e461223191ebb79
vastg <vastg@DESKTOP-EIVAT60>
2020-09-13 11:43:13 -0400

    a

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,55 +1,33 @@
 <template>
     <div>
-        <nav>
-            <v-toolbar dark prominent :src="src">
-                <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
-                <v-toolbar-title class="grey--text">
-                    <!--<span class="font-weight-light">Site</span>-->
-                    <span>{{siteTitle}}</span>
-                </v-toolbar-title>
-                <v-spacer></v-spacer>
+        <v-toolbar app dark prominent :src="src">
+            <v-app-bar-nav-icon class="grey--text hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title class="grey--text">
+                <!--<span class="font-weight-light">Site</span>-->
+                <span>{{siteTitle}}</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
 
-                <!-- dropdown menu -->
-                <v-menu offset-y>
-                    <template v-slot:activator="{ on }">
-                        <!-- <v-btn text slot="activator"> -->
-                        <v-btn text v-on="on">
-                            <v-icon left>expand_more</v-icon>
-                            <span>Menu</span>
-                        </v-btn>
-                    </template>
-                    <v-list>
-                        <!-- v-list-tile is changed to v-list-item -->
-                        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
-                            <v-list-item-title>{{ link.text }}</v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-
-                <v-btn text>
-                    <span>Login/Register</span>
-                    <!--<v-icon right>exit_to_app</v-icon>-->
+            <v-toolbar-title>
+                <v-btn text class="hidden-sm-and-down" v-for="link in links" :key="link.text" router :to="link.route">
+                    <span>{{ link.text }}</span>
                 </v-btn>
-                <!--<v-btn text>
-                    <span>Sign Out</span>
-                    <v-icon right>exit_to_app</v-icon>
-                </v-btn>-->
-            </v-toolbar>
-
-            <v-navigation-drawer v-model="drawer" absolute temporary app class="primary">
-                <v-list>
-                    <!-- v-list-tile is changed to v-list-item -->
-                    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
-                        <v-list-item-action>
-                            <v-icon class="white--text">{{ link.icon }}</v-icon>
-                        </v-list-item-action>
-                        <v-list-item-content>
-                            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
-                </v-list>
-            </v-navigation-drawer>
-        </nav>
+            </v-toolbar-title>
+        </v-toolbar>
+       
+        <v-navigation-drawer v-model="drawer" absolute temporary app class="primary">
+            <v-list>
+                <!-- v-list-tile is changed to v-list-item -->
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-item-action>
+                        <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
     </div>
 </template>
 
@@ -65,7 +43,8 @@
                 { icon: 'contacts', text: 'Gallery', route: '/gallery' },
                 { icon: 'contacts', text: 'Member Only', route: '/member' },
                 { icon: 'contacts', text: 'Admin', route: '/admin' },
-                { icon: 'contacts', text: 'Contact', route: '/contact' }
+                { icon: 'contacts', text: 'Contact', route: '/contact' },
+                { icon: 'contacts', text: 'Login/Register', route: '/loginRegister' }
             ]
         }),
     }
