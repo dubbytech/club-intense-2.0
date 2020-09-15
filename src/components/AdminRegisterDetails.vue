f70b349b03b59e29f9a5a2655abccf5d08b45b4d
vastg <vastg@DESKTOP-EIVAT60>
2020-09-15 18:08:46 -0400

    g

---
+++ src/components/AdminRegisterDetails.vue
@@ -0,0 +1,81 @@
+<template>
+    <v-form ref="form" v-model="valid" lazy-validation>
+        <v-container>
+            <v-row v-if="success">
+                <v-col cols="12">
+                    <v-alert dense type="success">
+                        Successfully updated.
+                    </v-alert>
+                </v-col>
+            </v-row>
+            <v-row v-if="error">
+                <v-col cols="12">
+                    <v-alert dense type="error">
+                        Error updating record.
+                    </v-alert>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="12" md="4">
+                    <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="12" md="4">
+                    <v-btn :disabled="!valid" color="success" class="mr-4" @click="registerMember">Register</v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+        <v-container>
+            <v-row>
+                <v-col cols="12">
+                    <v-card-text>
+                        <p class="text--primary">
+                            New registered member will receive an email to complete the registration and validation process.
+                        </p>
+                        <p class="text--primary">
+                            Please remind new registered member to be sure to check spam folders too for the email if not in inbox folder.
+                        </p>
+                    </v-card-text>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            valid: false,
+            email: "",
+            createdBy: 0,
+            createdTs: null,
+            changedBy: 0,
+            changedTs: null,
+            message: "",
+            success: false,
+            error: false,
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+/.test(v) || 'E-mail must be valid',
+            ]
+        }),
+        methods: {
+            validate() {
+                this.$refs.form.validate();
+            },
+            reset() {
+                this.$refs.form.reset();
+                this.valid = false;
+            },
+            resetValidation() {
+                this.$refs.form.resetValidation()
+            },
+            registerMember() {
+                alert("registered");
+                this.success = true;
+                this.error = false;
+            }
+        },
+    }
+</script>
